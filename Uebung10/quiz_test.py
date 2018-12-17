f99378fa3c034879d87b5fa775837a62c66eb558
PCA1610 <philippandre1@gmx.at>
2018-12-17 20:03:45 +0100

    Homework_10

---
+++ Uebung10/quiz_test.py
@@ -0,0 +1,9 @@
+from cityquiz import check_guess
+
+
+def test_check_guess():
+    assert check_guess("Ljubljana", "Slovenia", {"Slovenia": "Ljubljana"}) == True
+    return "test_check_guess() passed successfully."
+
+
+print test_check_guess()
